1928b4090ab7d093474573457120804b52bd9e94
taku oshida <40931117+pdoshida@users.noreply.github.com>
2019-04-09 01:28:35 +0900

    コメントアウト削除

--- logger.py
+++ logger.py
@@ -1,8 +1,8 @@
 #!/bin/python
 from pyzabbix import ZabbixAPI
-
 import sys
 import logging
+
 stream = logging.StreamHandler(sys.stdout)
 stream.setLevel(logging.DEBUG)
 log = logging.getLogger('pyzabbix')
@@ -10,10 +10,5 @@ log.addHandler(stream)
 log.setLevel(logging.DEBUG)
 
 zapi = ZabbixAPI("http://localhost/zabbix")
-zapi.login("zabbix user", "zabbix pasword")
-#print("Connected to Zabbix API Version %s" % zapi.api_version() + "\n")
-
-host = zapi.host.get(output="host")
-#host = zapi.host.get(output=["hostid", "host"], selectInterfaces=["ip"], selectGroups=["name"])
-#for h in host:
-#	print(h)
+zapi.login("Admin", "zabbix")
+print("Connected to Zabbix API Version %s" % zapi.api_version() + "\n")
