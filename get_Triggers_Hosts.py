9601edcfdf4627438e75ca96e68836221fd8c464
taku oshida <40931117+pdoshida@users.noreply.github.com>
2019-04-09 03:02:34 +0900

    first commit

---
+++ get_Triggers_Hosts.py
@@ -0,0 +1,46 @@
+#!/bin/python
+from pyzabbix import ZabbixAPI
+import csv
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+hostid = []
+hostname = []
+triggers=[]
+nomehosttrigger = []
+severidade = []
+
+dicionario = {'1':'Information', '2':'Warning', '3':'Average', '4':'High', '5':'Disaster'}
+
+zapi = ZabbixAPI('http://localhost/zabbix')
+zapi.login('Admin', 'zabbix')
+print(zapi.api_version())
+
+
+for l in zapi.host.get(output='extend'):
+    hostname.append(l['host'])
+
+triggers.append('Trigger')
+nomehosttrigger.append('Hostname')
+severidade.append('Severidade')
+
+
+for x in hostname:
+    for item in zapi.trigger.get(output='extend', filter={'host':x}):
+        nomehosttrigger.append(x)
+        triggers.append(item['description'])
+        severidade.append(dicionario[item['priority']])
+
+
+#data=pd.DataFrame({'Hostname':nomehosttrigger,'Triggers':triggers,'Severidade':severidade})
+#data.to_csv('triggers_hosts.csv',index=False)
+
+csv_out = open('Zabbix-Hosts_Triggers.csv','wb')
+
+mywriter = csv.writer(csv_out)
+
+for row in zip(nomehosttrigger,triggers,severidade):
+    mywriter.writerow(row)
+
+csv_out.close()
